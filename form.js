de38d8e39e41a41dd42e3fe0af7904760de555e0
LucasVigil <127551239+LucasVigil@users.noreply.github.com>
2023-06-20 19:19:00 -0300

    Entrega final con correcciones

--- form.js
+++ form.js
@@ -1,17 +1,66 @@
-document.getElementById("formularioContacto").addEventListener("submit", function(event) {   //con getelementbyid lo que hago es que en cada variable se vaya al html a buscar lo que hay almacenado con ese ID y lo almacene en la variable que quiero en el Js./ el AddEventListener hace que el js actue cuando detecta algun evento, en este caso cuando detecta que se toca el submit
-  event.preventDefault();
+document.getElementById("formularioContacto").addEventListener("submit", function(event) { //Addeventlistener sirve para que el codigo actue cuando detecta que se realiza un evento, en este caso cuando se apreta el botoncito de enviar.
+  event.preventDefault(); 
 
-  var nombre = document.getElementById("nombre").value; //var se usa para que la variable pueda ir almacenando mas de una cosa
-  var correo = document.getElementById("correo").value;
+  var nombre = document.getElementById("nombre").value; // con "var" puedo almacenar mas de un elemento en las variables, por lo que entendi es como poder armar las listas en python.
+  var correo = document.getElementById("correo").value;  //getelement by id hace que vaya al html y la variable guarde el string que encuentre con ese id.
   var mensaje = document.getElementById("mensaje").value;
 
-
-  document.getElementById("formularioContacto").style.display = "none"; //con esto hago que al momento de tocar el submit el fomrulario desaparezca
+  // Estas dos lineas cambian el display del formulario para que pueda saltar el mensaje de confirmacion de recepcion de la consulta.
+  document.getElementById("formularioContacto").style.display = "none";
   document.getElementById("mensajeEnviado").style.display = "block";
 
-  // esta funcion hace la inversa de lo anterios,  hace que el mensaje que salta con la confirmacion desaparezca solo a los 3 segs dejando que se pueda mandar otro mensaje
+  // Estos console logs hacen que la informacion de las consultas se quede guardada en la consola, porque es la unica forma que entendi para hacer que queden almacenados de alguna forma en algun lado.
+  console.log("Nombre: " + nombre);
+  console.log("Correo electrónico: " + correo);
+  console.log("Mensaje: " + mensaje);
+
+  // Esta funcion hace que a los 3 segs de enviarse la consulta y se muestre el mensaje de confirmacion, el usuario pueda volver a enviar una consulta
   setTimeout(function() {
     document.getElementById("formularioContacto").style.display = "block";
     document.getElementById("mensajeEnviado").style.display = "none";
+    document.getElementById("formularioContacto").reset();
   }, 3000);
 });
+
+function countdown() {
+  var now = new Date();
+  var openingTime = new Date(now.getFullYear(), now.getMonth(), now.getDate(), 9, 30, 0); // Establece la hora de apertura a las 9:30 AM porque son 2 horas mas que aca.
+  var closingTime = new Date(now.getFullYear(), now.getMonth(), now.getDate(), 16, 0, 0); // Establece la hora de cierre a las 4:00 PM 
+
+  var diff;
+
+  if (now >= openingTime && now <= closingTime) {
+    // Mercado abierto: calcular tiempo restante hasta el cierre
+    diff = closingTime - now;
+    document.body.style.backgroundColor = "#222";
+    document.body.style.color = "#fff";
+  } else {
+    // Mercado cerrado: calcular tiempo restante hasta la apertura
+    //esto funciona como el elif de python
+    if (now < openingTime) {
+      diff = openingTime - now;
+    } else {
+      openingTime.setDate(openingTime.getDate() + 1); // Calcula próxima apertura para tirar el tiempo hasta el dia siguiente.
+      diff = openingTime - now;
+    }
+    document.body.style.backgroundColor = "#333";
+    document.body.style.color = "#fff";
+  }
+
+  if (diff < 0) {
+    // Los mercados ya han abierto o cerrado
+    document.getElementById("countdown").innerHTML = "Los mercados ya han abierto o cerrado";
+    return;
+  }
+ //creando estas variables con este formato se hace que aparezcan las horas, despues los mins y despues los segundos.
+  var hours = Math.floor(diff / (1000 * 60 * 60));
+  var minutes = Math.floor((diff % (1000 * 60 * 60)) / (1000 * 60));
+  var seconds = Math.floor((diff % (1000 * 60)) / 1000);
+
+  document.getElementById("countdown").innerHTML = hours + "h " + minutes + "m " + seconds + "s";
+
+  setTimeout(countdown, 1000);
+}
+
+countdown();
+
